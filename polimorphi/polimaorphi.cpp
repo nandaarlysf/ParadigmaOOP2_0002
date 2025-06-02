dbaf8186489ab0b67c5da57781b3aaba4210b795
nandaarlysf <nandaarlys36@gmail.com>
2025-06-02 11:54:18 +0700

    int main polima

--- polimorphi/polimaorphi.cpp
+++ polimorphi/polimaorphi.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class seseorang {
 public:
-   vitual void pesan()=0;
+   virtual void pesan()=0;
    //virtual void pesan(){
   // cout<<"pesan dari seseorang"<<endl; 
   //}
@@ -14,9 +14,24 @@ class joko :public seseorang {
         cout<<"pesan dari joko"<<endl;
      }   
 };
+
 class lila:public seseorang {
 public:
      void pesan() {
         cout<<"pesan dari lila"<<endl;
      }   
 };
+int main(){
+    seseorang*obyek;
+    joko a;
+    lila b;
+    
+obyek=&a;
+    obyek->pesan();
+    obyek=&b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+    //b.seseorang::pesan();
+    
+    return 0;
+}
