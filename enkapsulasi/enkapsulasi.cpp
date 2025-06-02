afdd547448ff2a1147faaaf4abb86a523fc50dac
nandaarlysf <nandaarlys36@gmail.com>
2025-06-02 11:25:46 +0700

    remoteLampu rumah

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -12,3 +12,19 @@ public:
     return saklarNo[i];
    }
 };
+
+int main(){
+    remoteLampu lampuRumah;
+
+    lampuRumah.SetSaklarNo(0,"Lampu Teras Rumah");
+    lampuRumah.SetSaklarNo(1,"Lampu Ruang Tamu");
+    lampuRumah.SetSaklarNo(2,"Lampu kamar Tidur");
+     lampuRumah.SetSaklarNo(3,"Lampu Dapur");
+     
+   cout<<lampuRumah.getSaklarNo(0)<<endl;  
+   cout<<lampuRumah.getSaklarNo(1)<<endl;  
+    cout<<lampuRumah.getSaklarNo(2)<<endl;  
+     cout<<lampuRumah.getSaklarNo(3)<<endl;
+     
+     return 0;
+};
