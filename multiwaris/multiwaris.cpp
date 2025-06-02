f83a027b448c1cf127c3799647e48b27face6b75
nandaarlysf <nandaarlys36@gmail.com>
2025-06-02 11:32:04 +0700

    class orang

---
+++ multiwaris/multiwaris.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+class orang{
+public:
+   int umur;
+
+   orang(int pumur): umur(pumur){
+   cout << "orang dilihat dengan umur" << umur << "\n"
+   << endl;
+   }
+};
